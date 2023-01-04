30a08eff138fb9de1568d16cea36ee35f0d48e63
davidnavarrete <davidnava1998efrain@outlook.com>
2023-01-03 19:11:43 -0500

    Initial commit

---
+++ DnRepaso/Vistas/Inicio.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DnRepaso.Vistas
+{	
+	public partial class Inicio : ContentPage
+	{	
+		public Inicio ()
+		{
+			InitializeComponent ();
+		}
+
+       private void btnIniciar_Clicked(System.Object sender, System.EventArgs e)
+        {
+			string usuario = "Bienvenido "+ txtUsuario.Text;
+
+			Navigation.PushAsync(new Registro(usuario));
+        }
+    }
+}
+
---
+++ DnRepaso/Vistas/Registro.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DnRepaso.Vistas
+{	
+	public partial class Registro : ContentPage
+	{	
+		public Registro (string usuario)
+		{
+			InitializeComponent ();
+			lblUsuario.Text = usuario;
+
+		}
+
+       private void btnCalcular_Clicked(System.Object sender, System.EventArgs e)
+        {
+			try
+			{
+                double precio = Convert.ToDouble(txtPrecio.Text);
+                int cantidad = Convert.ToInt32(txtCantidad.Text);
+
+                double valor = precio * cantidad;
+                DisplayAlert("Valor Total del Producto", "El valor total es " + valor.ToString(), "Cerrar");
+            }
+			catch (Exception ex)
+			{
+				DisplayAlert("Alerta", ex.Message, "Cerra");
+			}
+			
+        }
+    }
+}
+
